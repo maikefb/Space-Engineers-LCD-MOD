cfaca3ecb426de4303369165174e9653e4fcb649
Arthur <GitHub@ArthurVSouza.com>
2025-09-20 16:44:51 -0300

    Change Title based on Filter on ItemCharts

--- Space-Engineers-LCD-MOD/Helpers/ItemCategoryHelper.cs
+++ Space-Engineers-LCD-MOD/Helpers/ItemCategoryHelper.cs
@@ -29,5 +29,28 @@ namespace Space_Engineers_LCD_MOD.Helpers
 
             return groupName;
         }
+        
+        public static string GetGroupDisplayName(string groupName)
+        {
+            switch (groupName)
+            {
+                case "AmmoMagazine":
+                    return MyTexts.GetString(MyStringId.GetOrCompute("DisplayName_BlueprintClass_Ammo"));
+                case "Component":
+                    return MyTexts.GetString(MyStringId.GetOrCompute("DisplayName_BlueprintClass_Components"));
+                case "PhysicalGun":
+                    return MyTexts.GetString(MyStringId.GetOrCompute("DisplayName_BlueprintClass_Tools"));    
+                case "Ingot":
+                    return MyTexts.GetString(MyStringId.GetOrCompute("Description_BlueprintClass_Ingots"));
+                case "Ore":
+                    return MyTexts.GetString(MyStringId.GetOrCompute("RadialMenuGroupTitle_VoxelOres"));
+                case "ConsumableItem":
+                    return MyTexts.GetString(MyStringId.GetOrCompute("DisplayName_BlueprintClass_Consumables"));
+                case "SeedItem":
+                    return MyTexts.GetString(MyStringId.GetOrCompute("DisplayName_BlueprintClass_GardenItems"));
+            }
+
+            return groupName;
+        }
     }
 }
