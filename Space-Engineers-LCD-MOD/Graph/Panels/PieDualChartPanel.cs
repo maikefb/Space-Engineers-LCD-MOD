3b753e931ebaf62cc09162cc9775ce71a04b1356
Arthur <GitHub@ArthurVSouza.com>
2025-09-21 19:56:29 -0300

    Fixed Dual Pie rendering incorrectly above 50%
    @ Fixed a bug that caused the first value of the Dual Pie not render correctly due to the second value not taking into account the transparency

--- Space-Engineers-LCD-MOD/Graph/Panels/PieDualChartPanel.cs
+++ Space-Engineers-LCD-MOD/Graph/Panels/PieDualChartPanel.cs
@@ -22,14 +22,70 @@ namespace Graph.Data.Scripts.Graph.Panels
             _sprites.Clear();
 
             if (_showTitle) DrawTitle(value, color.Value);
-            DrawBackground(value2, color.Value, _surface.ScriptForegroundColor, turnDarkOnComplete);
-            if (value2 > .99)
-                return _sprites;
+            DrawBackground(Math.Max(value, value2),
+                (value > value2) ? _surface.ScriptForegroundColor : color.Value, _surface.ScriptForegroundColor, turnDarkOnComplete);
 
-            DrawPie(value, _surface.ScriptForegroundColor, _surface.ScriptForegroundColor);
-            DrawPie(value2, color.Value, _surface.ScriptForegroundColor);
+            if(value > 0 && value > value2) // draw only if > 0 and bigger than the second value
+                DrawPie(value, _surface.ScriptForegroundColor, _surface.ScriptForegroundColor);
+            
+            if(value2 > 0 && value2 < .99)  // draw only if > 0 and not 100% (turnDarkOnComplete already draws 100%)
+                DrawPieWithTransparency(value2, color.Value);
 
             return _sprites;
+        }
+
+        protected virtual void DrawPieWithTransparency(float value, Color color)
+        {
+            Vector2 position = _origo - (_size / 2);
+
+            float deg = 360 * value;
+            float flip = value < 0.5f ? 1 : -1;
+
+            if (value > .99)
+                return;
+
+            float val = value < 0.5f ? 180 : 0;
+
+            // Cover 1
+            var semiCircle = new MySprite
+            {
+                Type = SpriteType.TEXTURE,
+                Data = "SemiCircle",
+                Position = position,
+                Size = _size,
+                Color = color,
+                RotationOrScale = MathHelper.ToRadians((flip * 90) + deg - val),
+                Alignment = TextAlignment.LEFT
+            };
+
+            if (value < 0.5f)
+            {
+                _sprites.Add(new MySprite(SpriteType.CLIP_RECT, null,
+                    new Vector2(position.X + _size.X / 2, position.Y - _size.Y / 2),
+                    _size // the X is bigger, but we don't care about width
+                ));
+                _sprites.Add(semiCircle);
+                _sprites.Add(MySprite.CreateClearClipRect());
+            }
+            else
+            {
+                _sprites.Add(semiCircle);
+            }
+
+            if (value <= 0.5f)
+                return;
+
+            // Cover 2
+            _sprites.Add(new MySprite
+            {
+                Type = SpriteType.TEXTURE,
+                Data = "SemiCircle",
+                Position = position,
+                Size = _size,
+                Color = color,
+                RotationOrScale = MathHelper.ToRadians(flip * (-90)),
+                Alignment = TextAlignment.LEFT
+            });
         }
     }
 }
